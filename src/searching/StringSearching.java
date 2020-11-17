b33c05402f57cd05206cdf4a7f2d098304e34fbe
A-Kyr <andreas.kyratzis.2019@gmail.com>
2020-11-17 17:15:56 +0200

    Implemented all string searching algorithms taught in CSC2032

--- src/searching/StringSearching.java
+++ src/searching/StringSearching.java
@@ -24,24 +24,64 @@ public class StringSearching {
     }
 
     public static int KMP(String text, String pattern) {
+        int[] next = getNextTable(pattern);
+        int t = text.length();
+        int p = pattern.length();
+
+        for (int i = 0; i <= (t - p); i++) {
+            int letter = 0;
+            while (letter < p && text.charAt(i + letter) == pattern.charAt(letter)) {
+                letter++;
+            }
+            if (letter == p) return i;
+            else i += (letter - next[letter] - 1);
+        }
         return -1;
     }
 
     private static int[] getNextTable(String str) {
-        return null;
+        int[] next_table = new int[str.length()];
+        next_table[0] = -1;
+        int pos = 1;
+        int cnd = 0;
+
+        while (pos < str.length()) {
+            if (str.charAt(pos) == str.charAt(cnd))
+                next_table[pos] = next_table[cnd];
+            else {
+                next_table[pos] = cnd;
+                cnd = next_table[cnd];
+                while (cnd >= 0 && str.charAt(pos) != str.charAt(cnd)) {
+                    cnd = next_table[cnd];
+                }
+            }
+            pos++;
+            cnd++;
+        }
+        return next_table;
     }
 
-    private static String getNextChar(char c) {
-        // COMPLICATED CODE AHEAD
-        // (int) temp.charAt(i)) -> ascii value of char at i
-        // (((int) temp.charAt(i)) + 1) -> next ascii value after i (we ensure its a different value)
-        // (char)(((int) temp.charAt(i)) + 1) -> cast that ascii value to char
-        // Character.toString((char)(((int) temp.charAt(i)) + 1)) -> make that char a string
-        return Character.toString((char) (((int) c) + 1));
+    public static int BoyerMoore(String text, String pattern) {
+        int t = text.length();
+        int p = pattern.length();
+        int i = 0;
+
+        while (i < (t - p)) {
+            int letter = p-1;
+            while (letter > 0 && text.charAt(i + letter) == pattern.charAt(letter)) {
+                letter--;
+            }
+            if (letter == 0) return i;
+            i += p;
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
-        System.out.println(getNextChar('a'));
+        String text = "ABBDABAEABBCD";
+        String str = "ABBC";
+
+        System.out.println(BoyerMoore(text, str));
     }
 
 }
